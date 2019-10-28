430bb56d21d336b2896d980710cd1ae783cf9514
cyril.alvarez <cyril@2getherbank.com>
2019-10-28 13:14:11 +0100

    added switch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import AppBar from './components/AppBar';
+import Switch from './components/Switch';
 
 import './App.css';
 
 function App() {
+  const [state, setState] = React.useState({
+    theme: true,
+  });
+
+  const theme = (el) => {
+    setState({theme: el})
+  }
+
   return (
-    <div className="App">
+  <div className={state.theme ? "App" : "App_Light"}>
       <AppBar />
 
-      <div className="container">
+      <div className={state.theme ? "container" : "container_ligth"}>
         <h1>DeFi the Odds</h1>
         <h5>Crypto Decentralised ETFs</h5>
         <h6>coming soon</h6>
       </div>
-      
+      <Switch theme={theme}/>
     </div>
   );
 }
