db880df7a007927c731d5c14be438cc8e7859fa3
cyril.alvarez <cyril@2getherbank.com>
2019-11-08 01:57:05 +0100

    styling

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
 import './appBar.css';
 
@@ -17,7 +19,17 @@ const useStyles = makeStyles(theme => ({
 
 export default function ButtonAppBar() {
     const classes = useStyles();
+    
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
 
+    const handleMenu = event => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
         <div className={classes.root}>
             <AppBar position="static" style={{ background: 'transparent', boxShadow: 'none' }}>
@@ -33,11 +45,30 @@ export default function ButtonAppBar() {
                             <a href="">DETFs</a>
                         </Typography>
                         <Typography variant="h6" className="appText">
-                            <a href="">Documents</a>
-                        </Typography>
-                        <Typography variant="h6" className="appText">
                             <a href="">Company</a>
                         </Typography>
+                        <Typography
+                            onClick={handleMenu}  
+                            aria-haspopup="true"
+                            variant="h6"
+                            className="appText">
+                            Documents
+                        </Typography>
+                            <Menu
+                                anchorEl={anchorEl}
+                                getContentAnchorEl={null}
+                                anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                                  transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'center',
+                                  }}
+                                open={open}
+                                onClose={handleClose}
+                                MenuListProps={{ onMouseLeave: handleClose }}
+                            >
+                                <MenuItem className="list" onClick={handleClose}>WhitePaper</MenuItem>
+                                <MenuItem className="list" onClick={handleClose}>Smart Contracts</MenuItem>
+                            </Menu>
                         <Typography variant="h6" className="appText">
                             <a href="">Dapp</a>
                         </Typography>
