699d505c4e75da2acac5793e96a75e94a5ea4cd5
caa1982 <cyril@2getherbank.com>
2020-01-16 19:15:15 +0100

    margin card

--- src/pages/DETF.js
+++ src/pages/DETF.js
@@ -7,7 +7,6 @@ import Typography from '@material-ui/core/Typography';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import styled, { keyframes } from 'styled-components';
-import CircularProgress from '@material-ui/core/CircularProgress';
 
 import { FaFilePdf } from 'react-icons/fa';
 
@@ -68,7 +67,7 @@ const useStyles = makeStyles({
         minHeight: '30vh',
         color: "#f5f5f5",
         textAlign: "center",
-        marginBottom: "2vh",
+        margin: "2%",
 
         "@media (min-width: 320px) and (max-width: 1024px)": {
             minWidth: '50vw',
@@ -109,78 +108,77 @@ export default function DETF() {
 
     const [data, setData] = useState(null);
 
-    if(data === null){
+    if (data === null) {
         prices.then(data => setData(data))
     }
 
     return (
-
-        <Container>
-            {data === null ? <Logo src={require(`../assets/img/Logo_fav.png`)} alt="icon" /> :
-                <Card raised={true} className={classes.card}>
-                    <CardContent>
-                        <Typography color="textPrimary" gutterBottom variant="h4" component="h2">
-                            DeFier Uniswap 5A
+            <Container>
+                {data === null ? <Logo src={require(`../assets/img/Logo_fav.png`)} alt="icon" /> :
+                    <Card raised={true} className={classes.card}>
+                        <CardContent>
+                            <Typography color="textPrimary" gutterBottom variant="h4" component="h2">
+                                DeFier Uniswap 5A
                     </Typography>
-                        <Typography variant="h5" color="textSecondary" component="p">
-                            Ticker: DU5A
+                            <Typography variant="h5" color="textSecondary" component="p">
+                                Ticker: DU5A
                     </Typography>
-                        <Typography variant="h6" color="textSecondary" component="p">
-                            Next Vote: N/A
-                    </Typography>
-                    
-                    <AssetValue>
-                        <Typography variant="h6" color="textSecondary" component="p">
-                                Market Capitalization: N/A
-                        </Typography>
                             <Typography variant="h6" color="textSecondary" component="p">
-                                Net Asset Value: N/A
+                                Next Vote: N/A
+                    </Typography>
+
+                            <AssetValue>
+                                <Typography variant="h6" color="textSecondary" component="p">
+                                    Market Capitalization: N/A
                         </Typography>
-                   </AssetValue>
-                   
-                        <Composition>
-                            {data.map(el => {
-                                if(el.ticker ===  "XCHF"){
-                                    return null; 
-                                }
-                                return <div style={{marginBottom: "2%" }}>
-                                    <Typography variant="body1" color="textPrimary" component="p">{el.name} ({el.ticker})</Typography>
-                                    
-                                    <Image src={require(`../assets/img/${el.ticker.toLowerCase()}.png`)} alt="icon" />
-
-                                    <Typography variant="body2" color="textSecondary" component="p">${
-                                        el.ticker === "BAT" || el.ticker === "CDAI" ? el.USDlast.toFixed(4) : el.USDlast.toFixed(2)
-                                    }</Typography>
-                                    
-                                    <Typography variant="body2" color="textSecondary" component="p">DETF Weigth: N/A</Typography>
-                                </div>
-
-                            })}
-                        </Composition>
-                    <Network>
-                        <Typography variant="h6" color="textSecondary" component="p">
-                            MainNet: Not Launched
+                                <Typography variant="h6" color="textSecondary" component="p">
+                                    Net Asset Value: N/A
                         </Typography>
-                        <Typography variant="h6" color="textSecondary" component="p">
-                            TestNet: Not Launched
+                            </AssetValue>
+
+                            <Composition>
+                                {data.map(el => {
+                                    if (el.ticker === "XCHF") {
+                                        return null;
+                                    }
+                                    return <div style={{ marginBottom: "2%" }}>
+                                        <Typography variant="body1" color="textPrimary" component="p">{el.name} ({el.ticker})</Typography>
+
+                                        <Image src={require(`../assets/img/${el.ticker.toLowerCase()}.png`)} alt="icon" />
+
+                                        <Typography variant="body2" color="textSecondary" component="p">${
+                                            el.ticker === "BAT" || el.ticker === "CDAI" ? el.USDlast.toFixed(4) : el.USDlast.toFixed(2)
+                                        }</Typography>
+
+                                        <Typography variant="body2" color="textSecondary" component="p">DETF Weigth: N/A</Typography>
+                                    </div>
+
+                                })}
+                            </Composition>
+                            <Network>
+                                <Typography variant="h6" color="textSecondary" component="p">
+                                    MainNet: Not Launched
                         </Typography>
-                    </Network>
+                                <Typography variant="h6" color="textSecondary" component="p">
+                                    TestNet: Not Launched
+                        </Typography>
+                            </Network>
 
-                    <FaFilePdf color='#272343' style={{cursor: "pointer"}}/>
+                            <FaFilePdf color='#272343' style={{ cursor: "pointer" }} />
 
-                        <div>
-                            <MuiThemeProvider theme={ButtonColor}>
-                                <StyledButton style={{ color: "#272343" }} href='https://defier.vote/'>
+                            <div>
+                                <MuiThemeProvider theme={ButtonColor}>
+                                    <StyledButton style={{ color: "#272343" }} href='https://defier.vote/'>
                                         Vote Now
                                 </StyledButton>
                                     <StyledButton style={{ color: "#f5f5f5" }} variant="contained" href='https://defier.exchange'>
                                         Trade Now
                                 </StyledButton>
-                            </MuiThemeProvider>
-                        </div>
-                    </CardContent>
-                </Card>
+                                </MuiThemeProvider>
+                            </div>
+                        </CardContent>
+                    </Card>
             }
-        </Container>
+            </Container>
     );
 }
