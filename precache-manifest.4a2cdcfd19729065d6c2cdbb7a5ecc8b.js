04b56cd568cec7a669a8dfb7f7c77fd3c9705d67
caa1982 <cyrilalvarezadriaansen@gmail.com>
2020-10-20 01:28:08 +0200

    Updates

---
+++ precache-manifest.4a2cdcfd19729065d6c2cdbb7a5ecc8b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "246dbce1cdd0474e158748945411f5a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34b6dedd532a0dddc612",
+    "url": "/static/css/main.2aa787c6.chunk.css"
+  },
+  {
+    "revision": "7648faac1a1b3d579013",
+    "url": "/static/js/2.85909e22.chunk.js"
+  },
+  {
+    "revision": "34b6dedd532a0dddc612",
+    "url": "/static/js/main.d2294ec9.chunk.js"
+  },
+  {
+    "revision": "89dbd287cbf1894c21ef",
+    "url": "/static/js/runtime-main.d517cf63.js"
+  },
+  {
+    "revision": "396ad45a69a68194c896648c5b94cfbf",
+    "url": "/static/media/Claimwar.396ad45a.svg"
+  },
+  {
+    "revision": "a992bf6450828d54bad8c7cb486a37e5",
+    "url": "/static/media/Gilroy-Heavy.a992bf64.otf"
+  },
+  {
+    "revision": "ac29a758c5d2d12b4ccabde8d975bec6",
+    "url": "/static/media/Gilroy-Semibold.ac29a758.otf"
+  },
+  {
+    "revision": "063a345e910165c9aeff469d4cd11af5",
+    "url": "/static/media/Governance.063a345e.svg"
+  },
+  {
+    "revision": "e8efee929eb2218fdc84a27389f3968b",
+    "url": "/static/media/Muli.e8efee92.ttf"
+  },
+  {
+    "revision": "38760057cca6f0b6ae6dcbcdfc689d2b",
+    "url": "/static/media/SmartContract.38760057.svg"
+  },
+  {
+    "revision": "906c52b5355e9c73f8bc07dc83da3487",
+    "url": "/static/media/Whitepaper.906c52b5.svg"
+  },
+  {
+    "revision": "a2d73fdc50cbbbb19d5ee53891b4e466",
+    "url": "/static/media/homePage.a2d73fdc.png"
+  },
+  {
+    "revision": "a93fbeb795f87e84b0378d088850fc4d",
+    "url": "/static/media/logo_white.a93fbeb7.svg"
+  }
+]);
